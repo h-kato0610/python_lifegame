81922066300c4f3f767b05d297e12711f770a6e6
h-kato0610 <h-kato0610@gmail.com>
2023-04-07 20:29:05 +0900

    update classfy

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import config
 
 from calc import Calc
 from cell import Cell
+from log import Log
 
 def main():
     calc = Calc()
     cell = Cell()
+    log = Log()
+
     calc_cell = cell.get_cell()
     for i in range(config.GENERATION):
         print('Generation: ' + str(i))
         time.sleep(0.5)
         calc_cell = calc.next_cell(calc_cell)
         cell.cell_view(calc_cell)
+        log.save(calc_cell, i)
+        log.write()
 
 main() if __name__ == '__main__' else None
 
