eeed2668b72a23d6d13a0c65780e21f26e1a1fda
h-kato0610 <h-kato0610@gmail.com>
2023-06-10 10:51:30 +0900

    fix remove log

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import config
 
 from calc import Calc
 from cell import Cell
-from log import Log
 
 def main():
     calc = Calc()
     cell = Cell()
-    log = Log()
 
     calc_cell = cell.get_cell()
     for i in range(config.GENERATION):
@@ -18,8 +16,6 @@ def main():
         time.sleep(0.5)
         calc_cell = calc.next_cell(calc_cell)
         cell.cell_view(calc_cell)
-        log.save(calc_cell, i)
-        log.write()
 
 main() if __name__ == '__main__' else None
 
